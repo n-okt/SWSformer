99c8aec1b289b34e9148d7654a2d398469076f95
Nagayuki Okitsu <ngyk.okitsu@gmail.com>
2026-04-10 20:10:47 +0900

    Delete basicsr directory

--- basicsr/models/losses/__init__.py
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------
-# Modified from CGNet (https://github.com/Ascend-Research/CascadedGaze)
-# ------------------------------------------------------------------------
-from .losses import (L1Loss, MSELoss, PSNRLoss, MultiHeadPSNRLoss)
-
-__all__ = [
-    'L1Loss', 'MSELoss', 'PSNRLoss', 'MultiHeadPSNRLoss',
-]
